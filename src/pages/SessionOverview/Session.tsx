e19daa03d3a73aba44d28b36b3e3df9e40fc34e1
Jarl Sondre Sæther <jarl_sondre@hotmail.com>
2023-03-18 15:09:00 -0400

    add like container

--- src/pages/SessionOverview/Session.tsx
+++ src/pages/SessionOverview/Session.tsx
@@ -27,6 +27,7 @@ export interface SessionInterface {
   datetime: string;
   user: number;
   username: string;
+  liked_by_usernames: string[];
 }
 
 type Props = {
@@ -36,6 +37,15 @@ type Props = {
 export default function Session(props: Props) {
   let date = new Date(props.session.datetime);
   const user = useSelector((state: any) => state.user);
+  function get_likes_string(username: string) {
+    if (props.session.liked_by_usernames.length == 0) return "No likes yet";
+    let result = "Liked by " + props.session.liked_by_usernames[0];
+    if (props.session.liked_by_usernames.length > 1) {
+      result +=
+        " and " + (props.session.liked_by_usernames.length - 1) + " more";
+    }
+    return result;
+  }
   return (
     <div
       className={
@@ -63,6 +73,16 @@ export default function Session(props: Props) {
             return <ExerciseUnit key={key} exerciseUnit={exerciseUnit} />;
           })}
         </div>
+        {user.username !== props.session.username ? (
+          <div className="like-container">
+            <button className="like-button">Like</button>
+            {get_likes_string(user.username)}
+          </div>
+        ) : (
+          <div className="like-container personal-like-container">
+            {get_likes_string(user.username)}
+          </div>
+        )}
       </div>
     </div>
   );
