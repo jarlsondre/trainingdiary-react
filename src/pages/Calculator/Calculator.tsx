82ff4eccbb579c2b9aa90c601ed13329cfd9d8ba
Jarl Sondre Sæther <jarl_sondre@hotmail.com>
2023-04-01 16:38:59 -0400

    add 1rm calculator functionality

--- src/pages/Calculator/Calculator.tsx
+++ src/pages/Calculator/Calculator.tsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import "./calculator.css";
 
 interface Props {}
+
+function calculateOneRepMax(weight: number, repetitions: number) {
+  // Using the Epley formula
+  if (weight <= 0) return 0;
+  if (repetitions <= 0) return 0;
+  return Math.round(weight * (1 + repetitions / 30));
+}
 
 export default function Calculator(props: Props) {
   const {} = props;
 
-  return <div>Upcoming feature :))</div>;
+  const [weight, setWeight] = useState<number>(0);
+  const [repetitions, setRepetitions] = useState<number>(0);
+
+  return (
+    <div className="calculator-page-container">
+      <h1>One Rep Max Calculator</h1>
+      <div className="explanatory-text">
+        Calculate your estimated 1RM by inserting your numbers here:{" "}
+      </div>
+      <div>
+        Weight:{" "}
+        <input
+          onChange={(event: any) => {
+            setWeight(event.target.value);
+          }}
+          className="calculator-weight-input"
+          type="number"
+        ></input>
+      </div>
+      <div>
+        Reptitions:{" "}
+        <input
+          onChange={(event: any) => {
+            setRepetitions(event.target.value);
+          }}
+          className="calculator-repetition-input"
+          type="number"
+        ></input>
+      </div>{" "}
+      <div>Result: {calculateOneRepMax(weight, repetitions)}</div>
+    </div>
+  );
 }
