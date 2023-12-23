dab0c8002a26eea0f1ccd7c7fc91f4c4ea759cc1
Jarl Sondre Sæther <jarl_sondre@hotmail.com>
2023-12-23 16:46:36 +0100

    add field for exericse comment

--- src/pages/SessionDetail/ExerciseUnitDetail.tsx
+++ src/pages/SessionDetail/ExerciseUnitDetail.tsx
@@ -3,33 +3,68 @@ import SetDetail from "./SetDetail";
 import "./exerciseUnitDetail.css";
 import { connect, useSelector } from "react-redux";
 import { deleteExerciseUnit } from "../../actions/exerciseUnits";
+import { useState } from "react";
 
 function ExerciseUnitDetail(props: any) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleIsEditing = () => {
+    setIsEditing(!isEditing);
+  };
   const handleDeleteExercise = () => {
+    setIsEditing(false);
     props.onDeleteExerciseUnit(Number(props.exerciseUnit.id));
+  };
+  const handleUpdateExercise = () => {
+    setIsEditing(false);
+    console.log("update exercise");
   };
 
   return (
     <div key={props.exerciseUnit} className="exercise-unit-detail-container">
-      <div className="exercise-name-container">
-        <div>
+      <div className="exercise-info-container">
+        <div className="exercise-name-comment-container">
           <div className="exercise-name">
             {props.exerciseUnit.exercise_name}
           </div>
-          {props.exerciseUnit.comment && (
-            <span className="exercise-unit-comment">
-              ({props.exerciseUnit.comment})
+          {isEditing ? (
+            <div>
+              <input
+                type="text"
+                defaultValue={props.exerciseUnit.comment}
+              ></input>
+            </div>
+          ) : (
+            <span>
+              {props.exerciseUnit.comment && (
+                <span className="exercise-unit-comment">
+                  ({props.exerciseUnit.comment})
+                </span>
+              )}
             </span>
           )}
         </div>
-        {props.editable && (
-          <button
-            className="delete-exercise-button"
-            onClick={handleDeleteExercise}
-          >
-            Delete
-          </button>
-        )}
+        {props.editable &&
+          (isEditing ? (
+            <div className="exercise-button-container">
+              <button
+                className="update-exercise-button"
+                onClick={handleUpdateExercise}
+              >
+                Update
+              </button>
+              <button
+                className="delete-exercise-button"
+                onClick={handleDeleteExercise}
+              >
+                Delete
+              </button>
+            </div>
+          ) : (
+            <button className="edit-exercise-button" onClick={toggleIsEditing}>
+              ...
+            </button>
+          ))}
       </div>
       {props.exerciseUnit.set.length > 0 ? (
         [...props.exerciseUnit.set]
